709d6013a8bddc70fabcf645c6648ab6c0484492
Max Base <maxbasecode@gmail.com>
2022-02-04 15:15:34 +0330

    clean code

--- cubic.js
+++ cubic.js
@@ -6,12 +6,12 @@ const cubicSolve = (a, b, c, d) => {
     c /= a
     d /= a
 
-    var q, r, s, t, term1
+    let s, t, term1
     let r13
+    let q
 
     q = (3.0*c - (b*b))/9.0
-    r = -(27.0*d) + b*(9.0*c - 2.0*(b*b))
-    r /= 54.0
+    const r = (-(27.0*d) + b*(9.0*c - 2.0*(b*b))) / 54.0
 
     const discrim = q*q*q + r*r
 
@@ -35,28 +35,32 @@ const cubicSolve = (a, b, c, d) => {
         roots[2].real = roots[1].real = -term1
         term1 = Math.sqrt(3.0)*(-t + s)/2
 
-        roots[1].i = term1;
-        roots[2].i = -term1;
-        return roots;
+        roots[1].i = term1
+        roots[2].i = -term1
+        return roots
     }
-
     // The remaining options are all real
     // All roots real, at least two are equal.
-    if(discrim == 0) {
-        r13 = ((r < 0) ? -Math.pow(-r,(1.0/3.0)) : Math.pow(r,(1.0/3.0)));
-        roots[0].real = -term1 + 2.0*r13;
-        roots[2].real = roots[1].real = -(r13 + term1);
-        return roots;
+    else if(discrim === 0) {
+        if(r < 0) {
+            r13 = -Math.pow(-r,(1.0/3.0))
+        } else {
+            r13 = Math.pow(r,(1.0/3.0))
+        }
+        roots[0].real = -term1 + 2.0*r13
+        roots[2].real = roots[1].real = -(r13 + term1)
+        return roots
     }
-
     // Only option left is that all roots are real and unequal (to get here, q < 0)
-    q = -q
-    const dum1 = Math.acos(r/Math.sqrt(q*q*q))
-    r13 = 2.0*Math.sqrt(q)
-
-    roots[0].real = -term1 + r13*Math.cos(dum1/3.0)
-    roots[1].real = -term1 + r13*Math.cos((dum1 + 2.0*Math.PI)/3.0)
-    roots[2].real = -term1 + r13*Math.cos((dum1 + 4.0*Math.PI)/3.0)
-
-    return roots
+    else {
+        q = -q
+        const dum1 = Math.acos(r/Math.sqrt(q*q*q))
+        r13 = 2.0*Math.sqrt(q)
+    
+        roots[0].real = -term1 + r13*Math.cos(dum1/3.0)
+        roots[1].real = -term1 + r13*Math.cos((dum1 + 2.0*Math.PI)/3.0)
+        roots[2].real = -term1 + r13*Math.cos((dum1 + 4.0*Math.PI)/3.0)
+    
+        return roots
+    }
 }
